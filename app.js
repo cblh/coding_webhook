1900601f97a1e2f1ef0b032e5a48b8d410c49fd9
陈宝林 <burlinchen@burlinchen-MB0.local>
2015-12-30 01:17:30 +0800

    add push event handler

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var handler = createHandler({
     token: 'mytoken' // maybe there is no token
 })
 
+var config = {
+    projectPath :'/data/sites/install/coding_webhook',
+    afterPullCommand:'npm run start'
+}
+
 http.createServer(function(req, res) {
     handler(req, res, function(err) {
         res.statusCode = 404
@@ -26,6 +31,25 @@ handler.on('*', function(event) {
 
 handler.on('push', function(event) {
     console.log(event)
+    if(event.ref === 'master'){
+        process.exec('git pull', {'cwd':'/home/coding/workspace'},
+            function (error, stdout, stderr) {
+                console.log('stdout========================\n' + stdout);
+                console.log('stderr========================\n' + stderr);
+                if (error !== null) {
+                    res.send('<pre>fail!!!\n' + stdout + error + '</pre>');
+                } else {
+                    process.exec(config.afterPullCommand, {'cwd':config.projectPath},
+                        function(error, stdout, stderr){
+                            if (error !== null) {
+                                res.send('<pre>fail!!!\n' + stdout + error + '</pre>');
+                            } else {
+                                res.send('<pre>done!!!\n' + stdout + '</pre>');
+                            }
+                        });
+                }
+            });
+    }
 })
 
 handler.on('star', function(event) {
